986fd25f0e41b10414cf23927c8010f8863aac2b
Bryth <62302815+Brythzz@users.noreply.github.com>
2021-09-16 00:52:40 +0200

    Fix iOS bug

--- src/client/components/ThemeSelector.js
+++ src/client/components/ThemeSelector.js
@@ -9,7 +9,7 @@ export default {
 
     render({ themes, setTheme }) {
         return (
-            <div class="theme-selector">
+            <div class="theme-selector" onclick="">
                 <svg viewBox="0 0 24 24" fill="#fff"><path d="M12,2A10,10,0,1,0,22,12,10,10,0,0,0,12,2Zm0,17.5V4.5a7.5,7.5,0,0,1,0,15Z"/></svg>
                 { themes.map((theme, themeIdx) => (<div onClick={() => setTheme(themeIdx)} class={`color-selector ${theme}`}></div>))}
             </div>
